b4676a81ef6403e99dbccadd22b95a6c9a0301ec
Dimitar Hristoskov <mike_mail@mail.bg>
2015-07-18 10:20:47 +0300

    High Quality Classes homework

---
+++ HQClasses/Abstraction/IFigure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Abstraction
+{
+    /// <summary>
+    /// Interface for classes capable of creating Figures
+    /// </summary>
+    internal interface IFigure
+    {
+        /// <summary>
+        /// Calculate Figure perimeter
+        /// </summary>
+        /// <returns>floating - point value</returns>
+        double CalculatePerimeter();
+
+        /// <summary>
+        /// Calculate Figure Surface
+        /// </summary>
+        /// <returns>floating - point value</returns>
+        double CalculateSurface();
+    }
+}
