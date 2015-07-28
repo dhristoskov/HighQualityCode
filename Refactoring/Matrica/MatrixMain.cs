d650908b57da2951bc2dba10c2d0c22e88a2baf1
Dimitar Hristoskov <mike_mail@mail.bg>
2015-07-28 12:06:46 +0300

    Refactoring

---
+++ Refactoring/Matrica/MatrixMain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Matrix
+{
+    internal class MatrixMain
+    {
+        /// <summary>
+        /// Read a user input from a console
+        /// </summary>
+        /// <returns>integer number a matrixSize
+        /// dimension of 2D array</returns>
+        private static int ReadUserInput()
+        {
+            Console.WriteLine("Enter a positive number: ");
+            string input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentNullException(nameof(input), "Input string can not be empty or null!");
+            }
+
+            int matrixSize;
+            while (!int.TryParse(input, out matrixSize) || matrixSize < 0 || matrixSize > 100)
+            {
+                Console.WriteLine("You haven't entered a correct positive number");
+                input = Console.ReadLine();
+            }
+
+            return matrixSize;
+        }
+
+        /// <summary>
+        /// Print a 2D array
+        /// </summary>
+        /// <param name="matrix">2D integer array</param>
+        private static void PrintMatrix(int [,] matrix)
+        {
+            for (int row = 0; row <= matrix.GetUpperBound(0); row++)
+            {
+                for (int col = 0; col <= matrix.GetUpperBound(1); col++)
+                {
+                    Console.Write("|{0,3}|", matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            int matrixSize = ReadUserInput();
+            var matrix = MatrixTraversUtil.GenerateMatrix(matrixSize);
+            PrintMatrix(matrix);
+        }
+    }
+}
---
+++ Refactoring/Matrica/MatrixTraversUtil.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace Matrix
+{
+    internal static class MatrixTraversUtil
+    {
+        //Patterns
+        private static readonly int[] DirectionX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+        private static readonly int[] DirectionY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+
+        /// <summary>
+        /// Generates and fill a matrix with numbers 
+        /// </summary>
+        /// <param name="size">The size of rows and cols</param>
+        /// <returns>A matrix filled with numbers using specific pattern</returns>
+        public static int[,] GenerateMatrix(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            Cell startingCell = FindEmptyCell(matrix);
+            if (startingCell != null)
+            {
+                FillMatrix(matrix, startingCell);
+            }
+
+            return matrix;
+        }
+
+        /// <summary>
+        /// Search for first empty cell in 
+        /// 2D array
+        /// </summary>
+        /// <param name="matrix">2D array</param>
+        /// <returns>null if empty Cell does not exist,
+        /// or new Cell</returns>
+        private static Cell FindEmptyCell(int[,] matrix)
+        {
+            for (int row = 0; row <= matrix.GetUpperBound(0); row++)
+            {
+                for (int col = 0; col <= matrix.GetUpperBound(1); col++)
+                {
+                    if (matrix[row, col] == 0)
+                    {
+                        var emptyCell = new Cell(row, col);
+                        return emptyCell;
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Check if cell is usable, if it is inside 
+        /// the matrix and if its equale to zero
+        /// </summary>
+        /// <param name="matrix">2D array</param>
+        /// <param name="cell">struct Cell </param>
+        /// <returns>boolean value</returns>
+        private static bool IsCellUsable(int[,] matrix, Cell cell)
+        {
+            bool isCellUsable = cell.X >= 0 && cell.X < matrix.GetLength(0) && cell.Y >= 0
+                            && cell.Y < matrix.GetLength(1) && matrix[cell.X, cell.Y] == 0;
+
+            return isCellUsable;
+        }
+
+        /// <summary>
+        /// Check if next cell is usable
+        /// </summary>
+        /// <param name="matrix">2D array</param>
+        /// <param name="cell">struct Cell</param>
+        /// <param name="index">index of pattern array</param>
+        /// <returns>boolean value</returns>
+        private static bool IsNextCellUsable(int[,] matrix, Cell cell, int index)
+        {
+            var nextCell = new Cell()
+            {
+                X = cell.X + DirectionX[index],
+                Y = cell.Y + DirectionY[index]
+            };
+
+            return IsCellUsable(matrix, nextCell);
+        }
+
+        /// <summary>
+        /// Check if there is avaliable
+        /// move, (empty cell)
+        /// </summary>
+        /// <param name="matrix">2D array</param>
+        /// <param name="cell">struct Cell</param>
+        /// <param name="index">index of pattern array</param>
+        /// <returns>boolean value</returns>
+        private static bool HasAvailableMove(int[,] matrix, Cell cell, int index)
+        {
+            foreach (var element in DirectionX)
+            {
+                index = (index + 1) % DirectionX.Length;
+
+                if (IsNextCellUsable(matrix, cell, index))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Fill matrix with numbers
+        /// operation
+        /// </summary>
+        /// <param name="matrix">2D array</param>
+        /// <param name="startingCell">first empty cell</param>
+        private static void FillMatrix(int[,] matrix, Cell startingCell)
+        {
+            var cell = startingCell;
+            var index = 0;
+
+            while (IsCellUsable(matrix, cell))
+            {
+                matrix[cell.X, cell.Y] = cell.Value;
+
+                while (!IsNextCellUsable(matrix, cell, index) &&
+                       HasAvailableMove(matrix, cell, index))
+                {
+                    index = (index + 1) % DirectionX.Length;
+                }
+
+                cell.X += DirectionX[index];
+                cell.Y += DirectionY[index];
+                cell.Value++;
+            }
+
+            // Calls FillMatrix recursively with next empty cell if exists
+            var nextCell = FindEmptyCell(matrix);
+            if (nextCell != null)
+            {
+                nextCell.Value = cell.Value;
+                FillMatrix(matrix, nextCell);
+            }
+        }
+    }
+}
